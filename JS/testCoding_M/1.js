1434331eba133783c211bc31b545f7881452ac05
Your Name <leonliu1008@gmail.com>
2023-11-27 00:11:20 +0800

    150 done

---
+++ JS/testCoding_M/1.js
@@ -0,0 +1,25 @@
+const isSubsequence = (str1, str2) => {
+  if (str1.length == 0) {
+    return true;
+  }
+
+  let i = 0;
+  let j = 0;
+  while (j < str2.length) {
+    if (str1[i] == str2[j]) {
+      i++;
+      j++;
+    }
+    if ((i = str1.length)) {
+      return true;
+    }
+    j++;
+  }
+
+  return false;
+};
+
+console.log(isSubsequence("", "Hello")); // true
+console.log(isSubsequence("book", "brooklyn")); // true
+console.log(isSubsequence("CAATCGA", "TCAATCAGGATCCGCTGA")); // true
+console.log(isSubsequence("AATTAA", "TCAATCAGGATCCGCTGA")); // false
