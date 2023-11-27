99e7be522be9d4411fab20d23cce23f4bf251824
Your Name <leonliu1008@gmail.com>
2023-11-28 00:16:52 +0800

    home 160 done

---
+++ Chapter7/app3.js
@@ -0,0 +1,12 @@
+let Leon = {
+  name: "Leon",
+  greet() {
+    console.log(thie.name + "打招呼");
+  },
+  walk: function () {
+    console.log(this.name + "正在走路");
+  },
+};
+
+Leon.greet();
+Leon.walk();
