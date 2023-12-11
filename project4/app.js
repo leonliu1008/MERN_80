822d7e8056b90708187f08f63c20e38d42fecb52
leonliu1008 <leonliu1008@gmail.com>
2023-12-11 16:53:12 +0800

    office 1211

--- project4/app.js
+++ project4/app.js
@@ -87,7 +87,126 @@ credits.forEach((credit) => {
 });
 
 let addButton = document.querySelector(".plus-btn");
-addButton.addEventListener("click", () => {});
+addButton.addEventListener("click", () => {
+  let newForm = document.createElement("form");
+  let newDiv = document.createElement("div");
+  newDiv.classList.add("geader"); // 將class 命名為 grader
+
+  //  製作5個元素
+  //  5個元素--1
+  let newInput1 = document.createElement("input");
+  newInput1.setAttribute("type", "text");
+  newInput1.setAttribute("list", "opt");
+  // newInput1.style.width = "24%";
+  newInput1.classList.add("class-type");
+  //  5個元素--2
+  let newInput2 = document.createElement("input");
+  newInput2.setAttribute("type", "text");
+  newInput2.classList.add("class-number");
+  //  5個元素--3
+  let newInput3 = document.createElement("input");
+  newInput3.setAttribute("type", "number");
+  newInput3.setAttribute("min", "0");
+  newInput3.setAttribute("max", "6");
+  newInput3.classList.add("class-credit");
+  newInput3.addEventListener("change", () => {
+    setGPA(); // 因新增了一個課程,所以成績要重新計算
+  });
+
+  //  5個元素--4
+  // here is the select tag
+  let newSelect = document.createElement("select");
+  newSelect.classList.add("select");
+  var opt1 = document.createElement("option");
+  opt1.setAttribute("value", "");
+  let textNode1 = document.createTextNode("");
+  opt1.appendChild(textNode1);
+  var opt2 = document.createElement("option");
+  opt2.setAttribute("value", "A");
+  let textNode2 = document.createTextNode("A");
+  opt2.appendChild(textNode2);
+  var opt3 = document.createElement("option");
+  opt3.setAttribute("value", "A-");
+  let textNode3 = document.createTextNode("A-");
+  opt3.appendChild(textNode3);
+  var opt4 = document.createElement("option");
+  opt4.setAttribute("value", "B+");
+  let textNode4 = document.createTextNode("B+");
+  opt4.appendChild(textNode4);
+  var opt5 = document.createElement("option");
+  opt5.setAttribute("value", "B");
+  let textNode5 = document.createTextNode("B");
+  opt5.appendChild(textNode5);
+  var opt6 = document.createElement("option");
+  opt6.setAttribute("value", "B-");
+  let textNode6 = document.createTextNode("B-");
+  opt6.appendChild(textNode6);
+  var opt7 = document.createElement("option");
+  opt7.setAttribute("value", "C+");
+  let textNode7 = document.createTextNode("C+");
+  opt7.appendChild(textNode7);
+  var opt8 = document.createElement("option");
+  opt8.setAttribute("value", "C");
+  let textNode8 = document.createTextNode("C");
+  opt8.appendChild(textNode8);
+  var opt9 = document.createElement("option");
+  opt9.setAttribute("value", "C-");
+  let textNode9 = document.createTextNode("C-");
+  opt9.appendChild(textNode9);
+  var opt10 = document.createElement("option");
+  opt10.setAttribute("value", "D+");
+  let textNode10 = document.createTextNode("D+");
+  opt10.appendChild(textNode10);
+  var opt11 = document.createElement("option");
+  opt11.setAttribute("value", "D");
+  let textNode11 = document.createTextNode("D");
+  opt11.appendChild(textNode11);
+  var opt12 = document.createElement("option");
+  opt12.setAttribute("value", "D-");
+  let textNode12 = document.createTextNode("D-");
+  opt12.appendChild(textNode12);
+  var opt13 = document.createElement("option");
+  opt13.setAttribute("value", "F");
+  let textNode13 = document.createTextNode("F");
+  opt13.appendChild(textNode13);
+
+  //  newSelect 裡面新增option
+  newSelect.appendChild(opt1);
+  newSelect.appendChild(opt2);
+  newSelect.appendChild(opt3);
+  newSelect.appendChild(opt4);
+  newSelect.appendChild(opt5);
+  newSelect.appendChild(opt6);
+  newSelect.appendChild(opt7);
+  newSelect.appendChild(opt8);
+  newSelect.appendChild(opt9);
+  newSelect.appendChild(opt10);
+  newSelect.appendChild(opt11);
+  newSelect.appendChild(opt12);
+  newSelect.appendChild(opt13);
+
+  newSelect.addEventListener("change", (e) => {
+    setGPA();
+    changeColor();
+  });
+
+  //  5個元素--5
+  let newButton = document.createElement("button");
+  newButton.classList.add("trash-button");
+  let newItag = document.createElement("i");
+  newItag.classList.add("fas", "fa-trash"); // 因Itag的class="fas fa-trash"
+
+  //  新增 必須注意DOM的建立順序
+  newButton.appendChild(newItag);
+  newDiv.appendChild(newInput1);
+  newDiv.appendChild(newInput2);
+  newDiv.appendChild(newInput3);
+  newDiv.appendChild(newSelect);
+  newDiv.appendChild(newButton);
+  newForm.appendChild(newDiv);
+  document.querySelector(".all-inputs").appendChild(newForm);
+});
+
 /**
  *    function
  */
