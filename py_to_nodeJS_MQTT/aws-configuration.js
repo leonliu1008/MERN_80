922f387f0c9615c7f9c30158285bffc804c25013
Your Name <leonliu1008@gmail.com>
2024-01-03 21:24:34 +0800

    move py_node_mqtt folder

--- py_to_nodeJS_MQTT/aws-configuration.js
+++
@@ -1,7 +0,0 @@
-var awsConfiguration = {
-  poolId: "us-east-1:bacf789a-7f31-4700-bc5f-8a3c6c9e2dcd", // 'YourCognitoIdentityPoolId'
-  host: "a36geamrvk9dtw-ats.iot.us-east-1.amazonaws.com", // 'YourAwsIoTEndpoint', e.g. 'prefix.iot.us-east-1.amazonaws.com'
-  region: "us-east-1", // 'YourAwsRegion', e.g. 'us-east-1'
-};
-// module.exports = awsConfiguration;
-export default awsConfiguration;
