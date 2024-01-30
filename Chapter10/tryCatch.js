f6550a884e44fb5658d2f27b9c55c6a68834d9f7
leonliu1008 <leonliu1008@gmail.com>
2024-01-30 17:17:57 +0800

    ch10 done

--- Chapter10/tryCatch.js
+++ Chapter10/tryCatch.js
@@ -1,15 +1,55 @@
 try {
-  hello();
+  //   hello();
+  console.log(a);
   //   console.log("無錯誤");
 } catch (err) {
-  if (e instanceof ReferenceError) {
+  if (err instanceof ReferenceError) {
     // 可以使用instanceof 來判斷ErrorType
     console.log("發生ReferenceError");
+  } else if (err instanceof TypeError) {
+    console.log("發生TypeError");
+  } else {
+    console.log("發生其他error");
   }
+} finally {
+  console.log("不管有無錯誤都會執行");
 }
 
 console.log("****************************************");
 
+/**
+ * 客製化錯誤訊息
+ */
+// throw丟
+class NotArrayError extends TypeError {
+  constructor(message) {
+    super(message);
+  }
+  printSolution() {
+    return "將Type改成Array 再執行";
+  }
+}
+function sumArray(arr) {
+  if (!Array.isArray(arr)) {
+    // throw "參數並非array!!";
+    // throw new TypeError("參數並非array!!");
+    throw new NotArrayError("參數並非array");
+  }
+  let result = 0;
+  arr.forEach((element) => {
+    result += element;
+  });
+  return result;
+}
+
+try {
+  sumArray("hello"); //故意放非array type
+} catch (e) {
+  console.log(e);
+  console.log(e.printSolution());
+}
+
+console.log("****************************************");
 // instanceof operator
 class Person {
   constructor(name) {
