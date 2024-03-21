f31f61c0ec1af9d5af916383026d63d680a221d4
leonliu1008 <leonliu1008@gmail.com>
2024-03-21 11:26:58 +0800

    sorted folder

--- Ajax/nodeEventLoop.js
+++ Ajax/nodeEventLoop.js
@@ -1,32 +1,32 @@
 console.log("start-1"); // sync
 
 process.nextTick(function () {
-  console.log("nextTick2-5");
+  console.log("nextTick2-6");
 });
 
 setTimeout(function () {
-  console.log("setTimeout-8");
+  console.log("setTimeout-9");
 });
 
 new Promise(function (resolve, reject) {
   console.log("promise-2");
-  resolve(console.log("resolve-2.5"));
+  resolve(console.log("resolve-3"));
 }).then(function (result) {
   // 此時會被放到microTask
-  console.log("promise then-7");
+  console.log("promise then-8");
 });
 
 // IIFE
 (async function () {
-  console.log("async-3");
+  console.log("async-4");
 })();
 
 setImmediate(function () {
-  console.log("setImmediate-9");
+  console.log("setImmediate-10");
 });
 
 process.nextTick(function () {
-  console.log("nextTick-6");
+  console.log("nextTick-7");
 });
 
-console.log("end-4");
+console.log("end-5");
